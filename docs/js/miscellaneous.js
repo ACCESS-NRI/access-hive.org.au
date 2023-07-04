2fee86a4c295b3eaa950abab911651e7d525f613
atteggiani <davide.marchegiani@gmail.com>
2023-07-05 02:27:35 +1000

    Remove mkdocs in copyright

--- docs/js/miscellaneous.js
+++ docs/js/miscellaneous.js
@@ -1,6 +1,23 @@
-document$.subscribe(function() {
-    var tables = document.querySelectorAll("article table:not([class])")
-    tables.forEach(function(table) {
-      new Tablesort(table)
-    })
-  })
+// Add buttons at the top of each table column (when hovered) to sort it
+function sortTables() {
+  let tables = document.querySelectorAll("article table:not([class])");
+  tables.forEach(table => new Tablesort(table));
+}
+
+function removeMkDocs() {
+  let copyright = document.querySelector(".md-copyright");
+  for (let i=0; i<=copyright.childNodes.length; i++) {
+    let node = copyright.childNodes[i];
+    if (node?.textContent.includes('Made with') || node?.textContent.includes('Material for MkDocs')) {
+      i--;
+      copyright.removeChild(node);
+    }
+  }
+}
+
+function main() {
+  sortTables();
+  removeMkDocs();
+}
+
+window.onload = () => document$.subscribe(() => main());
