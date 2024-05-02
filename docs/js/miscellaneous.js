d53ca6a0fc0250c725576ebf5ba79e85561c02cd
atteggiani <davide.marchegiani@gmail.com>
2024-05-03 09:53:06 +1000

    Fix to function to change URLs

--- docs/js/miscellaneous.js
+++ docs/js/miscellaneous.js
@@ -6,7 +6,8 @@
 function changeAbsoluteUrls() {
   let url = window.location.href;
   if (
-    url.startsWith('https://access-hive.org.au/development-website/') ||
+    url.startsWith('https://access-hive.org.au/development-website/') 
+    ||
     url.startsWith('https://access-hive.org.au/pr-preview/')
   ) {
     let links = document.querySelectorAll('a[href^="/"],img[src^="/"]');
